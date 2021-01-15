7f232804de33afe79cf882fd1514dc45d7323ee1
Akshit <akshit.aggarwal.04081999@gmail.com>
2021-01-16 00:19:40 +0530

    deploy

---
+++ build/precache-manifest.2f404022cd503c5de124d06a81104de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd6173425db6b3f2b1f49d197dc7333",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "c9f1d3d19f8797e0eb01",
+    "url": "/KeeperApp/static/css/main.2acb4e03.chunk.css"
+  },
+  {
+    "revision": "055bb078048903412ba4",
+    "url": "/KeeperApp/static/js/2.2740918d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/KeeperApp/static/js/2.2740918d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f1d3d19f8797e0eb01",
+    "url": "/KeeperApp/static/js/main.e9c89e92.chunk.js"
+  },
+  {
+    "revision": "8ff7c96ef64879a6fbc8",
+    "url": "/KeeperApp/static/js/runtime-main.e1ddcced.js"
+  }
+]);
